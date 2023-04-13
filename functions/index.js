8e7a4e1e7a3699fac685995c63025af665ac6ad1
Riley Smith <riley@rileysmith.me>
2023-04-12 21:57:35 -0700

    does this fix it

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 export function onRequest(context) {
-    return new Response({}, {
+    return new Response("", {
         headers: {
             location: 'https://www.youtube.com/watch?v=CXArovLJ60A'
         }
